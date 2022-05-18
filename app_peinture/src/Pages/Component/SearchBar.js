e233304c70f722fb1052f3616ea181b613a92832
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-05-18 16:23:34 +0200

    remise en page pour le css

---
+++ app_peinture/src/Pages/Component/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// import { useState } from "react";
+const SearchBar = () => {
+  return (
+    <div className="ctn-SearchBar">
+      <input type="search" placeholder="🔎Chercher une maquette" />
+    </div>
+  );
+};
+
+export default SearchBar;
--- app_peinture/src/Pages/Home.js
+++ app_peinture/src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Component/Navbar";
-import Search_bar from "./Component/Search_bar";
+import SearchBar from "./Component/SearchBar";
 
 const Home = () => {
   return (
@@ -8,7 +8,7 @@ const Home = () => {
       <Navbar />
       <div className="cnt-Home">
         <h1>Application Pot de Peinture</h1>
-        <Search_bar />
+        <SearchBar />
       </div>
     </div>
   );
